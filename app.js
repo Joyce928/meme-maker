cde2ed50eb4e57724d96f15b9d9f64afc0bdfd8c
Joyce928 <92929006+Joyce928@users.noreply.github.com>
2023-05-21 22:04:41 +1000

    #2.5 Filling Mode (06:29)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
@@ -10,6 +11,7 @@ canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 
 let isPaining = false;
+let isFilling = true;
 
 function onMove(event) {
   if (isPaining) {
@@ -46,11 +48,30 @@ function onColorClick(event) {
   color.value = colorValue;
 }
 
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Draw";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Fill";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPaining);
 canvas.addEventListener("mouseleave", cancelPaining);
+canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
